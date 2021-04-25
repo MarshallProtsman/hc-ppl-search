fa1740d54580b16dcd8a0fe9843bb7fa2d0edc47
marshallprotsman <47481879+MarshallProtsman@users.noreply.github.com>
2021-04-24 22:21:39 -0400

    heroku deploy

--- api/app.js
+++ api/app.js
@@ -1,18 +1,33 @@
-const express = require('express');
-const logger = require('morgan');
-const bodyParser = require('body-parser')
-require('dotenv').config()
+const express = require("express");
+const logger = require("morgan");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+require("dotenv").config();
 
 const app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-require('./server/routes')(app);
-app.get('*', (req, res) => res.status(200).send ({
-    message: '🌎 Hello World'
-}));
+require("./server/routes")(app);
+// app.get("*", (req, res) =>
+//   res.status(200).send({
+//     message: "🌎 Hello World",
+//   })
+// );
+
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("/*", function (req, res) {
+  res.status(200).send({
+    message: "🌎 Hello Woooooorld",
+  });
+  res.sendFile(path.join(__dirname, "../build", "index.html"));
+});
+
+app.listen(9000);
 
 module.exports = app;
